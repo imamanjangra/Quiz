d5a2d34da40f3112f9f4bb1f8f1ac9195185cd94
imamanjangra <imamanjangra@email.com>
2025-07-15 23:39:13 +0530

    add nav bar

--- script.js
+++ script.js
@@ -18,3 +18,29 @@
 //         color.classList.add("light-btn");
 //     }
 // });
+
+// start 
+
+let background_btn = document.querySelector("#Box");
+let body = document.querySelector("body");
+let nav  = document.querySelector(".nav");
+let mode = "light"
+
+ background_btn.addEventListener('click' , ()=>{
+
+if(mode ===  "light"){
+    mode = "dark"
+    body.style.backgroundColor = "black"
+    background_btn.style.color = "#DDDDDD"
+    body.style.color = "#DDDDDD"
+   nav.style.boxShadow = "0 1px 10px #DDDDDD";
+
+}else{
+    mode = "light"
+    body.style.backgroundColor = "white"
+    background_btn.style.color = "black"
+    body.style.color = "black"
+    nav.style.boxShadow = "0 1px 10px black";
+
+}
+})
