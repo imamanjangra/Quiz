45c8fc282a8634efabb8fc583609b0c6f810c2fc
imamanjangra <imamanjangra@email.com>
2025-07-15 23:39:44 +0530

    add nav

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// let color = document.querySelector("#Box");
-// let body = document.querySelector("body");
-
-// let mode = "light";
-
-// color.addEventListener("click", () => {
-//     if (mode === "light") {
-//         mode = "dark";
-//         body.classList.remove("light");
-//         body.classList.add("dark");
-//         color.classList.remove("light-btn");
-//         color.classList.add("dark-btn");
-//     } else {
-//         mode = "light";
-//         body.classList.remove("dark");
-//         body.classList.add("light");
-//         color.classList.remove("dark-btn");
-//         color.classList.add("light-btn");
-//     }
-// });
 
 // start 
 
